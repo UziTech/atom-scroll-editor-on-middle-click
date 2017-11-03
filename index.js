36c174aa2cbb908772548169e3ef4f45b8193c3e
Tony Brix <tony@brix.ninja>
2017-11-03 10:35:47 -0500

    use styles

--- index.js
+++ index.js
@@ -11,25 +11,57 @@ export default {
 			return;
 		}
 		this.running = true;
+
 		const left = this.editor.getScrollLeft();
 		const top = this.editor.getScrollTop();
 		const right = this.editor.getScrollWidth() - this.editor.getWidth();
 		const bottom = this.editor.getScrollHeight() - this.editor.getHeight();
+
 		let nextLeft = left + ((this.currentX - this.x) / this.speed);
-		if (nextLeft < 0) {
-			nextLeft = 0;
-		} else if (nextLeft > right) {
-			nextLeft = right;
-		}
+		// nextLeft = (nextLeft > right ? right : nextLeft);
+		// nextLeft = (nextLeft < 0 ? 0 : nextLeft);
+
 		let nextTop = top + ((this.currentY - this.y) / this.speed);
-		if (nextTop < 0) {
-			nextTop = 0;
-		} else if (nextTop > bottom) {
-			nextTop = bottom;
-		}
+		// nextTop = (nextTop > bottom ? bottom : nextTop);
+		// nextTop = (nextTop < 0 ? 0 : nextTop);
+
+		let dir = "";
+		dir += (nextTop < top ? "n" : (nextTop > top ? "s" : ""));
+		dir += (nextLeft < left ? "w" : (nextLeft > left ? "e" : ""));
+		this.editor.setAttribute("data-dir", dir);
+
 		this.editor.setScrollLeft(nextLeft);
 		this.editor.setScrollTop(nextTop);
+
 		requestAnimationFrame(this.scrollEditor);
+	},
+
+	startScroll(editor, e) {
+		this.editor = editor;
+		this.editor.classList.add("scroll-editor-on-middle-click-editor");
+		this.dot.style.left = e.pageX + "px";
+		this.dot.style.top = e.pageY + "px";
+		this.dot.classList.add("show");
+		this.x = e.pageX;
+		this.y = e.pageY;
+		this.currentX = e.pageX;
+		this.currentY = e.pageY;
+		if (!this.running) {
+			window.addEventListener("mousemove", this.setCurrent, { capture: true, passive: true });
+			this.scrollEditor();
+		}
+	},
+
+	stopScroll() {
+		window.removeEventListener("mousemove", this.setCurrent, { capture: true, passive: true });
+		if (this.dot) {
+			this.dot.classList.remove("show");
+		}
+		if (this.editor) {
+			this.editor.removeAttribute("data-dir");
+			this.editor.classList.remove("scroll-editor-on-middle-click-editor");
+			this.editor = null;
+		}
 	},
 
 	setCurrent(e) {
@@ -39,44 +71,27 @@ export default {
 
 	windowClick(e) {
 		if (e.button === 1) {
-			const editor = e.target.closest("atom-text-editor");
+			const editor = e.target.closest("atom-text-editor:not([mini])");
 			if (editor) {
 				if (this.editor === editor) {
-					editor.style = "";
-					this.dot.style.display = "none";
-					this.editor = null;
-					window.removeEventListener("mousemove", this.setCurrent, { capture: true, passive: true });
+					this.stopScroll();
 				} else {
-					editor.style = "cursor: move !important;";
-					this.dot.style.left = (e.pageX - this.dotRadius) + "px";
-					this.dot.style.top = (e.pageY - this.dotRadius) + "px";
-					this.dot.style.display = "";
-					this.x = e.pageX;
-					this.y = e.pageY;
-					this.currentX = e.pageX;
-					this.currentY = e.pageY;
 					if (this.editor) {
-						this.editor.style = "";
-						this.editor = editor;
+						this.editor.classList.remove("scroll-editor-on-middle-click-editor");
 					}
-					this.editor = editor;
-					if (!this.running) {
-						window.addEventListener("mousemove", this.setCurrent, { capture: true, passive: true });
-						this.scrollEditor();
-					}
+					this.startScroll(editor, e);
 				}
-			} else if (this.editor) {
-				window.removeEventListener("mousemove", this.setCurrent, { capture: true, passive: true });
-				this.editor.style = "";
-				this.dot.style.display = "none";
-				this.editor = null;
+			} else {
+				this.stopScroll();
 			}
+		} else {
+			this.stopScroll();
 		}
 	},
 
 	createDot() {
 		this.dot = document.createElement("div");
-		this.dot.style = "cursor: move; display: none; position: absolute; background-color: #fff; width: " + (this.dotRadius * 2 + 1) + "px; height: " + (this.dotRadius * 2 + 1) + "px; border-radius: 50%;";
+		this.dot.classList.add("scroll-editor-on-middle-click-dot");
 		document.body.append(this.dot);
 		this.disposables.add(new Disposable(() => {
 			this.dot.remove();
@@ -91,21 +106,18 @@ export default {
 	activate() {
 		this.disposables = new CompositeDisposable();
 
-		this.disposables.add(atom.config.observe("scroll-editor-on-middle-click.speed", (value) => {
-			this.speed = value;
-		}));
-		this.disposables.add(atom.config.observe("scroll-editor-on-middle-click.dotRadius", (value) => {
-			this.dotRadius = value;
-		}));
-		this.createDot();
 		this.setCurrent = this.setCurrent.bind(this);
 		this.windowClick = this.windowClick.bind(this);
 		this.scrollEditor = this.scrollEditor.bind(this);
+
+		this.createDot();
+		this.disposables.add(atom.config.observe("scroll-editor-on-middle-click.speed", (value) => {
+			this.speed = value;
+		}));
 		window.addEventListener("click", this.windowClick, { capture: true, passive: true });
 		this.disposables.add(new Disposable(() => {
 			window.removeEventListener("click", this.windowClick, { capture: true, passive: true });
-			window.removeEventListener("mousemove", this.setCurrent, { capture: true, passive: true });
-			this.editor = null;
+			this.stopScroll();
 		}));
 	},
 
